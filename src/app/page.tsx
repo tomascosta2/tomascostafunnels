a8a4c059cc30146e6f74bb746c98a8103771e05d
tomas <tomascostapp@gmail.com>
2025-10-13 00:10:13 -0300

    d

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -145,11 +145,11 @@ export default function Home() {
           </div>
         </section>
       </div>
-      <section className="py-[100px] px-4 relative">
+      <section className="py-[60px] md:py-[100px] px-4 relative">
         <div className="absolute top-0 left-0 bg-[url('/images/bg-2.webp')] bg-cover w-full h-full opacity-70 md:bg-center bg-[-400px]">
         </div>
         <div className="relative flex md:flex-row flex-col gap-8 md:items-end max-w-[800px] mx-auto">
-          <img className="w-full md:max-w-[330px]" src="/images/tomas-costa-testimonial-2.png" />
+          <img className="md:block hidden w-full md:max-w-[330px]" src="/images/tomas-costa-testimonial-2.png" />
           <div className="text-white py-6">
             <h3 className="font-bold traking-[-1%] text-[32px] mb-4"><span className="text-[#] text-[48px] me-2">“</span>Subi El Precio Cag*n</h3>
             <p>
@@ -157,12 +157,15 @@ export default function Home() {
               <br /><br />
               Me encantó cómo quedó y representa exactamente lo que quería transmitir. Mil gracias! Feliz con el resultado. Y...”
             </p>
-            <div className="mt-8">
-              <div className="flex gap-2 items-center">
-                <p>@juampigardonio</p>
-                <svg aria-label="Verified" className="x1lliihq x1n2onr6" fill="rgb(0, 149, 246)" height="18" role="img" viewBox="0 0 40 40" width="18"><title>Verified</title><path d="M19.998 3.094 14.638 0l-2.972 5.15H5.432v6.354L0 14.64 3.094 20 0 25.359l5.432 3.137v5.905h5.975L14.638 40l5.36-3.094L25.358 40l3.232-5.6h6.162v-6.01L40 25.359 36.905 20 40 14.641l-5.248-3.03v-6.46h-6.419L25.358 0l-5.36 3.094Zm7.415 11.225 2.254 2.287-11.43 11.5-6.835-6.93 2.244-2.258 4.587 4.581 9.18-9.18Z" fill-rule="evenodd"></path></svg>
+            <div className="mt-8 flex gap-4 items-center">
+              <img className="block md:hidden size-[60px]" src="/images/tomas-costa-testimonial-mobile.png" />
+              <div>
+                <div className="flex gap-2 items-center">
+                  <p>@juampigardonio</p>
+                  <svg aria-label="Verified" className="x1lliihq x1n2onr6" fill="rgb(0, 149, 246)" height="18" role="img" viewBox="0 0 40 40" width="18"><title>Verified</title><path d="M19.998 3.094 14.638 0l-2.972 5.15H5.432v6.354L0 14.64 3.094 20 0 25.359l5.432 3.137v5.905h5.975L14.638 40l5.36-3.094L25.358 40l3.232-5.6h6.162v-6.01L40 25.359 36.905 20 40 14.641l-5.248-3.03v-6.46h-6.419L25.358 0l-5.36 3.094Zm7.415 11.225 2.254 2.287-11.43 11.5-6.835-6.93 2.244-2.258 4.587 4.581 9.18-9.18Z" fill-rule="evenodd"></path></svg>
+                </div>
+                <p>+600k seguidores</p>
               </div>
-              <p>+600k seguidores</p>
             </div>
           </div>
         </div>
