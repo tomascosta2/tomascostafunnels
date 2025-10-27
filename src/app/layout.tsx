a57af64979eeb70b5c4e5171507c94ee25d71359
tomas <tomascostapp@gmail.com>
2025-10-27 11:34:09 -0300

    d

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import '@fontsource-variable/montserrat';
 import '@fontsource-variable/inter';
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Tomás Costa Funnels - Embudos de venta para coaches fitness, nutricionistas y psicologos online",
@@ -18,6 +19,22 @@ export default function RootLayout({
     <html lang="en">
       <body>
         {children}
+        <Script
+          id="hotjar"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(h,o,t,j,a,r){
+                  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                  h._hjSettings={hjid:6558319,hjsv:6};
+                  a=o.getElementsByTagName('head')[0];
+                  r=o.createElement('script');r.async=1;
+                  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                  a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            `,
+          }}
+        />
       </body>
     </html>
   );
